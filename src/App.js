c3347643a32ea552485573517a86b4ad537910ef
Marco Guaspari Worms <marcogworms@gmail.com>
2023-02-04 20:33:40 -0300

    backup

--- src/App.js
+++ src/App.js
@@ -19,10 +19,20 @@ function App() {
           <img src={logo} />
           <h1>yGenius</h1>
           <h2>Support Assistant</h2>
+          <div>
+            <p style={{color:'grey'}}><b>Anon</b></p>
+            <p>When veYFI?</p>
+            <br />
+            <p style={{color:'blue'}}><b>yGenius</b></p>
+            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+          </div>
         </div>
       </main>
       
       <footer>
+        <a>
+          Regenerate Response
+        </a>
         <div>
           <img src={send} />
           <input type="text" placeholder="Ask here" />
