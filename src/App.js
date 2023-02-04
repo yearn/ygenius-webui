f800668d332206c817fec7f25109139e1c2b372e
Marco Guaspari Worms <marcogworms@gmail.com>
2023-02-04 20:11:25 -0300

    backup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
+import send from './send.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <span>yearn.fi</span>
+        </div>
+        <div>
+          <span>docs.yearn</span>
+        </div>
       </header>
+      
+      <main>
+        <div>
+          <img src={logo} />
+          <h1>yGenius</h1>
+          <h2>Support Assistant</h2>
+        </div>
+      </main>
+      
+      <footer>
+        <div>
+          <img src={send} />
+          <input type="text" placeholder="Ask here" />
+        </div>
+        <span>
+          GPT Index powered by yearn devdocs, articles, proposals, onchain data, and support channel history.
+        </span>
+      </footer>
     </div>
   );
 }
