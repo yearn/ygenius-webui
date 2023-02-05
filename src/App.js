5eb2a69c3d6487ad6648cc941bb0e400602f2596
Marco Guaspari Worms <marcogworms@gmail.com>
2023-02-05 18:47:56 -0300

    backup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import send from './send.svg'
 import './App.css'
+import { zip } from 'ramda'
 
 function App() {
   const [input, setInput] = useState('')
-  const [result, setResult] = useState('')
-  const [question, setQuestion] = useState('')
+  const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [isFirstRequest, setIsFirstRequest] = useState(true)
+
+  const generateResponse = (question, _questions, _answers) => {
+    if (_questions === undefined) { _questions = questions }
+    if (_answers === undefined) { _answers = answers }
+    setIsFirstRequest(() => false)
+    setIsLoading(() => true)
+    setQuestions(() => [..._questions, question])
+    fetch(`https://a1b2-2804-431-c7f3-9298-b9b2-78a-92d3-19f6.sa.ngrok.io/ask?query=${
+      encodeURIComponent(question.substring(0, 4000))
+    }&history=${
+      encodeURIComponent(zip(_questions, _answers)
+        .map(([q, a]) => `Question: ${q}\nAnswer:${a}`).join('\n\n'))
+        .substring(0, 4000)
+    }`)
+    .then((response) => response.text())
+    .then((data) => {
+      setAnswers(() => [..._answers, data.substring(1, data.length - 2).replace(/\\/g, '')])
+      // example wrong answer: The \"swappers\" thing.....
+      // make it not have \ before the ", so \"swappers\" -> "swappers"	
+      // console.log(data.replace(/\\n/g,'\n').replace(/^"|"$/g, '').replace(/"$/gm, ''))
+
+      setIsLoading(() => false)
+    });
+    setInput(() => '')
+  }
+
+  const scrollToBottom = () => {
+    const div = document.querySelector(".scrollable");
+    div.scrollTop = div.scrollHeight - div.clientHeight;
+  };
+  
+  useEffect(() => {
+    scrollToBottom();
+  }, [questions, answers]);
+
   return (
     <div className="App">
       <header>
@@ -23,34 +62,69 @@ function App() {
           <img src={logo} />
           <h1>yGenius</h1>
           <h2>Support Assistant</h2>
-          <div>
-            {question && <div>
-              <p style={{color:'grey'}}><b>Anon</b></p>
-              <p>{question}</p>
-            </div>}
-            {result && <div>
-              <p style={{color:'blue'}}><b>yGenius</b></p>
-              <p>{result}</p>
+          <div className='scrollable'>
+            {!isFirstRequest && <div>
+              {questions.map((question, index) => {
+                return (
+                  <div key={question}>
+                    <p style={{color:'#7E7E7E'}}><b>Anon</b></p>
+                    <p>{question}</p>
+                    <p style={{color:'#0657F9', marginTop: 30}}><b>yGenius</b></p>
+                    {answers[index] ? <div>
+                      <p>{answers[index]}</p>
+                    </div> : <div>
+                      <p className='loading-dots'><span>.</span><span>.</span><span>.</span></p>
+                    </div>}
+                  </div>
+                )
+              })}
             </div>}
           </div>
         </div>
       </main>
       
       <footer>
-        <a>
-          Regenerate Response
-        </a>
+        <div className="buttons">
+          <a onClick={() => {
+            if (isLoading || answers.length === 0 ) { return }
+            const lastQuestion = questions[questions.length - 1]
+            let _questions
+            let _answers
+
+            if (questions.length === 1) {
+              _questions = []
+              _answers = []
+            } else {
+              _questions = questions.slice(0, -1)
+              _answers = answers.slice(0, -1)
+            }
+
+            console.log(_questions, _answers)
+
+            generateResponse(lastQuestion, _questions, _answers)
+
+          }}>
+            Regenerate Response
+          </a>
+          <a style={{marginLeft: 20}} onClick={() => {
+            if (isLoading || answers.length === 0 ) { return }
+            setAnswers([])
+            setQuestions([])
+            setIsFirstRequest(() => true)
+          }}>
+            Clear All
+          </a>
+        </div>
         <div>
-          <img src={send} alt="" onClick={() => {
-            setQuestion(input)
-            fetch(`https://a1b2-2804-431-c7f3-9298-b9b2-78a-92d3-19f6.sa.ngrok.io/ask?query=${input}`)
-            .then((response) => response.text())
-            .then((data) => {
-              setResult(data.replace(/\\n/g,'\n').replace(/^"|"$/g, '').replace(/"$/gm, ''))
-            });
-            setInput('')
-          }} />
-          <input type="text" placeholder="Ask here" value={input} onChange={e => { setInput(e.target.value) }} />
+          <img src={send} alt="" onClick={() => generateResponse(input)} />
+          <input type="text" placeholder="Ask here" value={input} autoFocus
+            onChange={e => { setInput(() => e.target.value.substring(0, 4000))}}
+            onKeyDown={e => {
+              if (e.key === 'Enter') {
+                generateResponse(input)
+              }
+            }}
+          />
         </div>
         <span>
           GPT Index powered by yearn devdocs, articles, proposals, onchain data, and support channel history.
